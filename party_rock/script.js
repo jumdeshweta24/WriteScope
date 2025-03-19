d428c152e30409b62cbcc2bf911185dbced3e0f6
jumdeshweta24 <140889524+jumdeshweta24@users.noreply.github.com>
2025-03-19 14:56:37 +0530

    Add files via upload

---
+++ party_rock/script.js
@@ -0,0 +1,3 @@
+function redirectToPartyRock() {
+    window.location.href = "https://partyrock.aws/u/shwetajumde/fAqcvBijb/WriteScope";  // Update this with the correct Party Rock link if needed
+}
